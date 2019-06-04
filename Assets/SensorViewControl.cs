2b40ca74d2ea82c565ccf5ba9351a1195d2bc8e0
Soós Róbert <totalse95@gmail.com>
2019-06-04 14:10:27 +0200

    Initial commit

---
+++ Assets/SensorViewControl.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+#if UNITY_UWP
+using System;
+using Windows.Media.Capture.Frames;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using System.Runtime.InteropServices.WindowsRuntime;
+#endif
+
+public class SensorViewControl : MonoBehaviour {
+
+    private Texture2D tex = null;
+    private byte[] bytes = null;
+
+    private int count = 300;
+
+    private KeywordRecognizer keywordRecognizer;
+
+    [SerializeField]
+    public string[] keywords;
+
+    void Start() {
+        keywordRecognizer = new KeywordRecognizer(keywords);
+        keywordRecognizer.OnPhraseRecognized += OnPhraseRecognized;
+        keywordRecognizer.Start();
+
+        Debug.Log("Starting task");
+#if UNITY_UWP
+        Task.Run(() => { InitSensor(); });
+#endif
+    }
+
+	void Update () {
+		//if (count == 0) {
+        //    SendFrameData();
+        //    count = 300;
+		//}
+        //count--;
+	}
+
+#if UNITY_UWP
+    private async void InitSensor()
+    {
+        var mediaFrameSourceGroupList = await MediaFrameSourceGroup.FindAllAsync();
+        var mediaFrameSourceGroup = mediaFrameSourceGroupList[0];
+        var mediaFrameSourceInfo = mediaFrameSourceGroup.SourceInfos[0];
+        MediaFrameSourceKind kind = mediaFrameSourceInfo.SourceKind;
+        var mediaCapture = new MediaCapture();
+        var settings = new MediaCaptureInitializationSettings()
+        {
+            SourceGroup = mediaFrameSourceGroup,
+            SharingMode = MediaCaptureSharingMode.SharedReadOnly,
+            StreamingCaptureMode = StreamingCaptureMode.Video,
+            MemoryPreference = MediaCaptureMemoryPreference.Cpu,
+        };
+        try
+        {
+            await mediaCapture.InitializeAsync(settings);
+            var mediaFrameSource = mediaCapture.FrameSources[mediaFrameSourceInfo.Id];
+            MediaFrameReader mediaframereader;
+            if (kind == MediaFrameSourceKind.Color)
+            {
+                mediaframereader = await mediaCapture.CreateFrameReaderAsync(mediaFrameSource, MediaEncodingSubtypes.Argb32);
+            }
+            else
+            {
+                mediaframereader = await mediaCapture.CreateFrameReaderAsync(mediaFrameSource, mediaFrameSource.CurrentFormat.Subtype);
+            }
+            //var mediaframereader = await mediaCapture.CreateFrameReaderAsync(mediaFrameSource, mediaFrameSource.CurrentFormat.Subtype);
+            mediaframereader.FrameArrived += FrameArrived;
+            await mediaframereader.StartAsync();
+        }
+        catch (Exception e)
+        {
+            UnityEngine.WSA.Application.InvokeOnAppThread(() => { Debug.Log(e); }, true);
+        }
+    }
+
+    private void FrameArrived(MediaFrameReader sender, MediaFrameArrivedEventArgs args)
+    {
+        var mediaframereference = sender.TryAcquireLatestFrame();
+        if (mediaframereference != null)
+        {
+            var videomediaframe = mediaframereference?.VideoMediaFrame;
+            var softwarebitmap = videomediaframe?.SoftwareBitmap;
+            if (softwarebitmap != null)
+            {
+                softwarebitmap = SoftwareBitmap.Convert(softwarebitmap, BitmapPixelFormat.Rgba8, BitmapAlphaMode.Premultiplied);
+                int w = softwarebitmap.PixelWidth;
+                //Debug.Log("w: " + w);
+                int h = softwarebitmap.PixelHeight;
+                //Debug.Log("h: " + h);
+                if (bytes==null)
+                {
+                    bytes = new byte[w * h * 4];
+                }
+                softwarebitmap.CopyToBuffer(bytes.AsBuffer());
+                softwarebitmap.Dispose();
+                UnityEngine.WSA.Application.InvokeOnAppThread(() => {
+                    if (tex == null)
+                    {
+                        tex = new Texture2D(w, h, TextureFormat.RGBA32, false);
+                        GetComponent<Renderer>().material.mainTexture = tex;
+                    }
+                    for (int i = 0; i < bytes.Length / 4; ++i)
+                    {
+                        byte b = bytes[i * 4];
+                        bytes[i * 4 + 0] = 0;
+                        bytes[i * 4 + 1] = 0;
+                        bytes[i * 4 + 2] = 0;
+                        bytes[i * 4 + 3] = 255;
+                        if (b==0)
+                        {
+                            bytes[i * 4 + 0] = 128;
+                        }
+                        if (b==1)
+                        {
+                            bytes[i * 4 + 0] = 255;
+                        }
+                    }
+                    tex.LoadRawTextureData(bytes);
+                    tex.Apply();
+                }, true);
+            }
+            mediaframereference.Dispose();
+        }
+    }
+#endif
+
+    public void SendFrameData() {
+        Debug.Log("DATA SENT");
+#if UNITY_UWP
+        this.GetComponent<UDPClient>().SendByteData(bytes);
+#endif
+    }
+    private void OnPhraseRecognized(PhraseRecognizedEventArgs args) {
+        Debug.Log("KEYWORD");
+        SendFrameData();
+    }
+}
---
+++ Assets/UDPClient.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine.Windows.Speech;
+
+#if !UNITY_EDITOR
+using Windows.Networking;
+using Windows.Networking.Sockets;
+using System.IO;
+#endif
+
+public class UDPClient : MonoBehaviour {
+
+    [Header("Local")]
+    [SerializeField]
+    private int localPort;
+
+    [Header("Remote")]
+    [SerializeField]
+    private string remoteIP;
+    [SerializeField]
+    public string remotePort;
+
+    IPEndPoint remoteEndPoint;
+    //UdpClient client;  
+
+    public void Start() {
+        //Init();
+    }
+
+    /*
+    public void Init() {
+        print("UDPSend.init()");
+
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(remoteIP), remotePort);
+        //client = new UdpClient();
+
+        print("Sending to " + remoteIP + " : " + remotePort);
+        print("Testing: nc -lu " + remoteIP + " : " + remotePort);
+    }
+
+    public void SendByteData(byte[] data) {
+        try {
+            //client.Send(data, data.Length, remoteEndPoint);
+        }
+        catch (Exception err) {
+            print(err.ToString());
+        }
+    }
+
+    public void SendString(string message) {
+        byte[] data = Encoding.UTF8.GetBytes(message);
+        SendByteData(data);
+    }
+    */
+
+#if !UNITY_EDITOR
+    public async void SendByteData(byte[] data) {
+        StreamSocket socket = new StreamSocket();
+
+        HostName host = new HostName(remoteIP); //Replace with coorect hostname when running on RPi
+
+        try {
+            try {
+                await socket.ConnectAsync(host, remotePort);
+            }
+            catch(Exception ex) {
+                Debug.Log("HIBA");
+                //txb_Events.Text += ex.Message;
+            }
+
+            Stream streamOut = socket.OutputStream.AsStreamForWrite();
+            StreamWriter writer = new StreamWriter(streamOut);
+            //BinaryWriter writer = new BinaryWriter(streamOut);
+            string request = System.Text.Encoding.ASCII.GetString(data, 0, data.Length);
+            //string request = BitConverter.ToString(data);
+            Debug.Log(data[0] + "," + data[1] + "," + data[2] + "," + data[3] + "," + data[4] + "," + data[5]);
+            Debug.Log(request);
+            await writer.WriteLineAsync(request);
+            //await writer.WriteAsync((char) data);
+            //await writer.Write(data);
+            //writer.Write(data);
+            await writer.FlushAsync();
+
+            socket.Dispose();
+
+        }
+        catch (Exception ex) {
+            //txb_Events.Text += ex.Message;
+            //Logs.Add(ex.Message)
+        }
+    }
+#endif
+}
